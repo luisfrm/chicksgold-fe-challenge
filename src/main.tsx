9ed7321da6d66e07e611484e092fa375890c156b
Luis Rivas <luisfrm_@outlook.com>
2025-01-29 00:51:40 -0400

    added css to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./styles/dropdown.css";
+import "./styles/cartButton.css";
+import "./styles/header.css";
+import "./styles/currencyButton.css";
+import "./styles/sidebar.css";
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
