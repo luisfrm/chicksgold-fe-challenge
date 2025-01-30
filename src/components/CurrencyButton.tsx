a77ae3098d89020b718b0c6a8a8f1be0187d1302
Luis Rivas <luisfrm_@outlook.com>
2025-01-30 04:09:18 -0400

    removed unnecesary code from currency button

--- src/components/CurrencyButton.tsx
+++ src/components/CurrencyButton.tsx
@@ -1,18 +1,6 @@
+import { menuItems } from "../config/config";
 import { Dropdown, DropdownContent, DropdownItem, DropdownMenu } from "./Dropdown";
-import { ChevronDown, Gamepad2, Crosshair, Box, Target } from "lucide-react";
-
-interface MenuItem {
-	id: string;
-	label: string;
-	icon?: React.ReactNode;
-}
-
-const menuItems: MenuItem[] = [
-	{ id: "usd", label: "USD", icon: <Target className="h-4 w-4" /> },
-	{ id: "cop", label: "COP", icon: <Crosshair className="h-4 w-4" /> },
-	{ id: "eur", label: "EUR", icon: <Crosshair className="h-4 w-4" /> },
-	{ id: "ves", label: "VES", icon: <Gamepad2 className="h-4 w-4" /> },
-	{ id: "gbp", label: "GBP", icon: <Box className="h-4 w-4" /> },];
+import { ChevronDown} from "lucide-react";
 
 const CurrencyButton = () => {
 	return (
