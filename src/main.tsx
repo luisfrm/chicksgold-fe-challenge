8feaee52aacc9b072ed61c4e525e6e94a0663bec
Luis Rivas <luisfrm_@outlook.com>
2025-01-30 00:42:34 -0400

    added styles to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/variables.css";
+import "./styles/index.css";
+import "./styles/global.css";
 import "./styles/dropdown.css";
 import "./styles/cartButton.css";
 import "./styles/header.css";
 import "./styles/currencyButton.css";
 import "./styles/sidebar.css";
+import "./styles/products.css";
+import "./styles/pagination.css";
+import "./styles/pagination.css";
+import "./styles/customSelect.css";
 
-import App from './App.tsx'
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
